5520df6c5b3fd8ab750167b1c964f37c0182f880
Ben Lynn <benlynn@gmail.com>
2017-11-27 18:47:28 -0800

    Started interpreter.

--- wasm.hs
+++ wasm.hs
@@ -1,25 +1,29 @@
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 import Control.Monad
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
 import Data.Char
+import Data.Int
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IM
+import Data.List
 import Data.Word
 import Text.Parsec.ByteString
 import Text.Parsec
-import Text.Printf
 
 data Type = I32 | I64 | F32 | F64 | Func | AnyFunc | Nada deriving (Show, Eq)
 data ExternalKind = Function | Table | Memory | Global
 data FuncType = FuncType [Type] [Type] deriving Show
 data Import = Import String String FuncType deriving Show
-data Body = Body [Type] [String] deriving Show
+data Body = Body [Type] [Op] deriving Show
 data Wasm = Wasm {
   types :: [FuncType],
   imports :: [Import],
   decls :: [Int],
   memory :: [(Int, Maybe Int)],
-  globals :: [((Type, Bool), [String])],
+  globals :: [((Type, Bool), [Op])],
   exports :: [(String, Int)],
   start :: Maybe Int,
   code :: [Body] } deriving Show
@@ -27,12 +31,21 @@ data Wasm = Wasm {
 emptyWasm :: Wasm
 emptyWasm = Wasm [] [] [] [] [] [] Nothing []
 
-zeroOperandOps :: [(Int, String)]
+data Op = I32_eqz | I32_eq | I32_ne | I32_lt_s | I32_lt_u | I32_gt_s | I32_gt_u | I32_le_s | I32_le_u | I32_ge_s | I32_ge_u | I64_eqz | I64_eq | I64_ne | I64_lt_s | I64_lt_u | I64_gt_s | I64_gt_u | I64_le_s | I64_le_u | I64_ge_s | I64_ge_u | F32_eq | F32_ne | F32_lt | F32_gt | F32_le | F32_ge | F64_eq | F64_ne | F64_lt | F64_gt | F64_le | F64_ge | I32_clz | I32_ctz | I32_popcnt | I32_add | I32_sub | I32_mul | I32_div_s | I32_div_u | I32_rem_s | I32_rem_u | I32_and | I32_or | I32_xor | I32_shl | I32_shr_s | I32_shr_u | I32_rotl | I32_rotr | I64_clz | I64_ctz | I64_popcnt | I64_add | I64_sub | I64_mul | I64_div_s | I64_div_u | I64_rem_s | I64_rem_u | I64_and | I64_or | I64_xor | I64_shl | I64_shr_s | I64_shr_u | I64_rotl | I64_rotr | F32_abs | F32_neg | F32_ceil | F32_floor | F32_trunc | F32_nearest | F32_sqrt | F32_add | F32_sub | F32_mul | F32_div | F32_min | F32_max | F32_copysign | F64_abs | F64_neg | F64_ceil | F64_floor | F64_trunc | F64_nearest | F64_sqrt | F64_add | F64_sub | F64_mul | F64_div | F64_min | F64_max | F64_copysign | I32_wrap_i64 | I32_trunc_s_f32 | I32_trunc_u_f32 | I32_trunc_s_f64 | I32_trunc_u_f64 | I64_extend_s_i32 | I64_extend_u_i32 | I64_trunc_s_f32 | I64_trunc_u_f32 | I64_trunc_s_f64 | I64_trunc_u_f64 | F32_convert_s_i32 | F32_convert_u_i32 | F32_convert_s_i64 | F32_convert_u_i64 | F32_demote_f64 | F64_convert_s_i32 | F64_convert_u_i32 | F64_convert_s_i64 | F64_convert_u_i64 | F64_promote_f32 | I32_reinterpret_f32 | I64_reinterpret_f64 | F32_reinterpret_i32 | F64_reinterpret_i64
+  | I32_load Int Int | I64_load Int Int | F32_load Int Int | F64_load Int Int | I32_load8_s Int Int | I32_load8_u Int Int | I32_load16_s Int Int | I32_load16_u Int Int | I64_load8_s Int Int | I64_load8_u Int Int | I64_load16_s Int Int | I64_load16_u Int Int | I64_load32_s Int Int | I64_load32_u Int Int | I32_store Int Int | I64_store Int Int | F32_store Int Int | F64_store Int Int | I32_store8 Int Int | I32_store16 Int Int | I64_store8 Int Int | I64_store16 Int Int | I64_store32 Int Int
+  | Unreachable | Nop | Else | End | Return
+  | Block Type | Loop Type | If Type
+  | Get_local Int | Set_local Int | Tee_local Int | Get_global Int | Set_global Int
+  | I32_const Int32 | I64_const Int64 | F32_const Float | F64_const Double
+  | Br_table [Int] Int
+  | Br Int | Br_if Int
+  | Call Int deriving Show
+
+zeroOperandOps :: [(Int, Op)]
 zeroOperandOps = cmpOps ++ ariOps ++ crops where
-  -- Copied from webpage and piped through a script:
-  cmpOps = [(0x45, "i32.eqz"), (0x46, "i32.eq"), (0x47, "i32.ne"), (0x48, "i32.lt_s"), (0x49, "i32.lt_u"), (0x4a, "i32.gt_s"), (0x4b, "i32.gt_u"), (0x4c, "i32.le_s"), (0x4d, "i32.le_u"), (0x4e, "i32.ge_s"), (0x4f, "i32.ge_u"), (0x50, "i64.eqz"), (0x51, "i64.eq"), (0x52, "i64.ne"), (0x53, "i64.lt_s"), (0x54, "i64.lt_u"), (0x55, "i64.gt_s"), (0x56, "i64.gt_u"), (0x57, "i64.le_s"), (0x58, "i64.le_u"), (0x59, "i64.ge_s"), (0x5a, "i64.ge_u"), (0x5b, "f32.eq"), (0x5c, "f32.ne"), (0x5d, "f32.lt"), (0x5e, "f32.gt"), (0x5f, "f32.le"), (0x60, "f32.ge"), (0x61, "f64.eq"), (0x62, "f64.ne"), (0x63, "f64.lt"), (0x64, "f64.gt"), (0x65, "f64.le"), (0x66, "f64.ge")]
-  ariOps = [(0x67, "i32.clz"), (0x68, "i32.ctz"), (0x69, "i32.popcnt"), (0x6a, "i32.add"), (0x6b, "i32.sub"), (0x6c, "i32.mul"), (0x6d, "i32.div_s"), (0x6e, "i32.div_u"), (0x6f, "i32.rem_s"), (0x70, "i32.rem_u"), (0x71, "i32.and"), (0x72, "i32.or"), (0x73, "i32.xor"), (0x74, "i32.shl"), (0x75, "i32.shr_s"), (0x76, "i32.shr_u"), (0x77, "i32.rotl"), (0x78, "i32.rotr"), (0x79, "i64.clz"), (0x7a, "i64.ctz"), (0x7b, "i64.popcnt"), (0x7c, "i64.add"), (0x7d, "i64.sub"), (0x7e, "i64.mul"), (0x7f, "i64.div_s"), (0x80, "i64.div_u"), (0x81, "i64.rem_s"), (0x82, "i64.rem_u"), (0x83, "i64.and"), (0x84, "i64.or"), (0x85, "i64.xor"), (0x86, "i64.shl"), (0x87, "i64.shr_s"), (0x88, "i64.shr_u"), (0x89, "i64.rotl"), (0x8a, "i64.rotr"), (0x8b, "f32.abs"), (0x8c, "f32.neg"), (0x8d, "f32.ceil"), (0x8e, "f32.floor"), (0x8f, "f32.trunc"), (0x90, "f32.nearest"), (0x91, "f32.sqrt"), (0x92, "f32.add"), (0x93, "f32.sub"), (0x94, "f32.mul"), (0x95, "f32.div"), (0x96, "f32.min"), (0x97, "f32.max"), (0x98, "f32.copysign"), (0x99, "f64.abs"), (0x9a, "f64.neg"), (0x9b, "f64.ceil"), (0x9c, "f64.floor"), (0x9d, "f64.trunc"), (0x9e, "f64.nearest"), (0x9f, "f64.sqrt"), (0xa0, "f64.add"), (0xa1, "f64.sub"), (0xa2, "f64.mul"), (0xa3, "f64.div"), (0xa4, "f64.min"), (0xa5, "f64.max"), (0xa6, "f64.copysign")]
-  crops = [(0xa7, "i32.wrap/i64"), (0xa8, "i32.trunc_s/f32"), (0xa9, "i32.trunc_u/f32"), (0xaa, "i32.trunc_s/f64"), (0xab, "i32.trunc_u/f64"), (0xac, "i64.extend_s/i32"), (0xad, "i64.extend_u/i32"), (0xae, "i64.trunc_s/f32"), (0xaf, "i64.trunc_u/f32"), (0xb0, "i64.trunc_s/f64"), (0xb1, "i64.trunc_u/f64"), (0xb2, "f32.convert_s/i32"), (0xb3, "f32.convert_u/i32"), (0xb4, "f32.convert_s/i64"), (0xb5, "f32.convert_u/i64"), (0xb6, "f32.demote/f64"), (0xb7, "f64.convert_s/i32"), (0xb8, "f64.convert_u/i32"), (0xb9, "f64.convert_s/i64"), (0xba, "f64.convert_u/i64"), (0xbb, "f64.promote/f32"), (0xbc, "i32.reinterpret/f32"), (0xbd, "i64.reinterpret/f64"), (0xbe, "f32.reinterpret/i32"), (0xbf, "f64.reinterpret/i64")]
+  cmpOps = [(0x45, I32_eqz), (0x46, I32_eq), (0x47, I32_ne), (0x48, I32_lt_s), (0x49, I32_lt_u), (0x4a, I32_gt_s), (0x4b, I32_gt_u), (0x4c, I32_le_s), (0x4d, I32_le_u), (0x4e, I32_ge_s), (0x4f, I32_ge_u), (0x50, I64_eqz), (0x51, I64_eq), (0x52, I64_ne), (0x53, I64_lt_s), (0x54, I64_lt_u), (0x55, I64_gt_s), (0x56, I64_gt_u), (0x57, I64_le_s), (0x58, I64_le_u), (0x59, I64_ge_s), (0x5a, I64_ge_u), (0x5b, F32_eq), (0x5c, F32_ne), (0x5d, F32_lt), (0x5e, F32_gt), (0x5f, F32_le), (0x60, F32_ge), (0x61, F64_eq), (0x62, F64_ne), (0x63, F64_lt), (0x64, F64_gt), (0x65, F64_le), (0x66, F64_ge)]
+  ariOps = [(0x67, I32_clz), (0x68, I32_ctz), (0x69, I32_popcnt), (0x6a, I32_add), (0x6b, I32_sub), (0x6c, I32_mul), (0x6d, I32_div_s), (0x6e, I32_div_u), (0x6f, I32_rem_s), (0x70, I32_rem_u), (0x71, I32_and), (0x72, I32_or), (0x73, I32_xor), (0x74, I32_shl), (0x75, I32_shr_s), (0x76, I32_shr_u), (0x77, I32_rotl), (0x78, I32_rotr), (0x79, I64_clz), (0x7a, I64_ctz), (0x7b, I64_popcnt), (0x7c, I64_add), (0x7d, I64_sub), (0x7e, I64_mul), (0x7f, I64_div_s), (0x80, I64_div_u), (0x81, I64_rem_s), (0x82, I64_rem_u), (0x83, I64_and), (0x84, I64_or), (0x85, I64_xor), (0x86, I64_shl), (0x87, I64_shr_s), (0x88, I64_shr_u), (0x89, I64_rotl), (0x8a, I64_rotr), (0x8b, F32_abs), (0x8c, F32_neg), (0x8d, F32_ceil), (0x8e, F32_floor), (0x8f, F32_trunc), (0x90, F32_nearest), (0x91, F32_sqrt), (0x92, F32_add), (0x93, F32_sub), (0x94, F32_mul), (0x95, F32_div), (0x96, F32_min), (0x97, F32_max), (0x98, F32_copysign), (0x99, F64_abs), (0x9a, F64_neg), (0x9b, F64_ceil), (0x9c, F64_floor), (0x9d, F64_trunc), (0x9e, F64_nearest), (0x9f, F64_sqrt), (0xa0, F64_add), (0xa1, F64_sub), (0xa2, F64_mul), (0xa3, F64_div), (0xa4, F64_min), (0xa5, F64_max), (0xa6, F64_copysign)]
+  crops = [(0xa7, I32_wrap_i64), (0xa8, I32_trunc_s_f32), (0xa9, I32_trunc_u_f32), (0xaa, I32_trunc_s_f64), (0xab, I32_trunc_u_f64), (0xac, I64_extend_s_i32), (0xad, I64_extend_u_i32), (0xae, I64_trunc_s_f32), (0xaf, I64_trunc_u_f32), (0xb0, I64_trunc_s_f64), (0xb1, I64_trunc_u_f64), (0xb2, F32_convert_s_i32), (0xb3, F32_convert_u_i32), (0xb4, F32_convert_s_i64), (0xb5, F32_convert_u_i64), (0xb6, F32_demote_f64), (0xb7, F64_convert_s_i32), (0xb8, F64_convert_u_i32), (0xb9, F64_convert_s_i64), (0xba, F64_convert_u_i64), (0xbb, F64_promote_f32), (0xbc, I32_reinterpret_f32), (0xbd, I64_reinterpret_f64), (0xbe, F32_reinterpret_i32), (0xbf, F64_reinterpret_i64)]
 
 wasm :: Parser Wasm
 wasm = do
@@ -168,49 +181,54 @@ wasm = do
         pure $ Body locals ops
       pure w { code = bodies}
 
-    codeBlock :: Int -> Parser [String]
+    codeBlock :: Int -> Parser [Op]
     codeBlock lvl = do
       opcode <- varuint7
       s <- if
-        | Just s <- lookup opcode $ zeroOperandOps ++ [(0x00, "unreachable"), (0x01, "nop"), (0x05, "else"), (0x0b, "end"), (0x0f, "return")] -> pure s
-        | Just s <- lookup opcode [(0x02, "block"), (0x03, "loop"), (0x04, "if")] -> do
+        | Just s <- lookup opcode $ zeroOperandOps ++ [(0x00, Unreachable), (0x01, Nop), (0x05, Else), (0x0b, End), (0x0f, Return)] -> pure s
+        | Just s <- lookup opcode [(0x02, Block), (0x03, Loop), (0x04, If)] -> do
           bt <- blockType
-          pure $ s ++ " " ++ show bt
-        | Just s <- lookup opcode [(0x20, "get_local"), (0x21, "set_local"), (0x22, "tee_local"), (0x23, "get_global"), (0x24, "set_global")] -> do
+          pure $ s bt
+        | Just s <- lookup opcode [(0x20, Get_local), (0x21, Set_local), (0x22, Tee_local), (0x23, Get_global), (0x24, Set_global)] -> do
           v <- varuint32
-          pure $ s ++ " " ++ show v
-        | Just s <- lookup opcode [(0x28, "i32.load"), (0x29, "i64.load"), (0x2a, "f32.load"), (0x2b, "f64.load"), (0x2c, "i32.load8_s"), (0x2d, "i32.load8_u"), (0x2e, "i32.load16_s"), (0x2f, "i32.load16_u"), (0x30, "i64.load8_s"), (0x31, "i64.load8_u"), (0x32, "i64.load16_s"), (0x33, "i64.load16_u"), (0x34, "i64.load32_s"), (0x35, "i64.load32_u"), (0x36, "i32.store"), (0x37, "i64.store"), (0x38, "f32.store"), (0x39, "f64.store"), (0x3a, "i32.store8"), (0x3b, "i32.store16"), (0x3c, "i64.store8"), (0x3d, "i64.store16"), (0x3e, "i64.store32")] -> do
+          pure $ s v
+        | Just s <- lookup opcode [(0x28, I32_load), (0x29, I64_load), (0x2a, F32_load), (0x2b, F64_load), (0x2c, I32_load8_s), (0x2d, I32_load8_u), (0x2e, I32_load16_s), (0x2f, I32_load16_u), (0x30, I64_load8_s), (0x31, I64_load8_u), (0x32, I64_load16_s), (0x33, I64_load16_u), (0x34, I64_load32_s), (0x35, I64_load32_u), (0x36, I32_store), (0x37, I64_store), (0x38, F32_store), (0x39, F64_store), (0x3a, I32_store8), (0x3b, I32_store16), (0x3c, I64_store8), (0x3d, I64_store16), (0x3e, I64_store32)] -> do
           flags <- varuint32
           offset <- varuint32
-          pure $ s ++ " " ++ show flags ++ " " ++ show offset
-        | Just s <- lookup opcode [(0x0c, "br"), (0x0d, "br_if")] -> do
+          pure $ s flags offset
+        | Just s <- lookup opcode [(0x0c, Br), (0x0d, Br_if)] -> do
           v <- varuint32
-          pure $ s ++ " " ++ show v
+          pure $ s v
         | otherwise -> case opcode of
           0x0e -> do
             n <- varuint32
             tgts <- replicateM n varuint32
             defTgt <- varuint32
-            pure $ "br_table " ++ show tgts ++ " " ++ show defTgt
+            pure $ Br_table tgts defTgt
           0x41 -> do
             i32 <- varint32
-            pure $ "i32.const " ++ show i32
+            pure $ I32_const $ fromIntegral i32
           0x42 -> do
             i64 <- varint64
-            pure $ "i64.const " ++ show i64
+            pure $ I64_const $ fromIntegral i64
           0x44 -> do
             u64 <- uint64
-            pure $ "f64.const " ++ printf "%016x" u64
+            pure $ F64_const 0  -- TODO: Convert from u64.
           0x10 -> do
             i <- varuint32
-            pure $ "call " ++ show i
+            pure $ Call i
           _ -> fail ("bad opcode " ++ show opcode)
       if
-        | s == "end" -> if lvl == 0
+        | End <- s -> if lvl == 0
           then pure [s]
           else (s:) <$> codeBlock (lvl - 1)
-        | head (words s) `elem` ["if", "block", "loop"] -> (s:) <$> codeBlock (lvl + 1)
+        | blockBegin s -> (s:) <$> codeBlock (lvl + 1)
         | otherwise -> (s:) <$> codeBlock lvl
+        where
+          blockBegin (If _) = True
+          blockBegin (Block _) = True
+          blockBegin (Loop _) = True
+          blockBegin _ = False
 
     sect w = do
       n <- varuint7
@@ -241,4 +259,65 @@ main = do
   s <- B.getContents
   case parse wasm "" s of
     Left err -> putStrLn $ "parse error: " ++ show err
-    Right out -> print out
+    Right out -> execute out "e"
+
+data VM = VM
+  { globs :: IntMap Op
+  , stack :: [Op]
+  , insts :: [[Op]]
+  , mem :: IntMap Word8
+  } deriving Show
+
+putNum :: Integral a => Int -> Int32 -> a -> IntMap Word8 -> IntMap Word8
+putNum w addr n m = foldl' f m [0..w-1] where
+  f m k = IM.insert (fromIntegral addr + k) (getByte (w-1 - k)) m
+  getByte k = fromIntegral $ fromIntegral n `div` (256^k) `mod` 256
+
+getNum :: Int -> Int32 -> IntMap Word8 -> Integer
+getNum w addr m = sum
+  [fromIntegral (m IM.! (fromIntegral addr + k)) * 2^(w-1 - k) | k <- [0..w-1]]
+
+execute w@Wasm {imports, exports, code, globals} s = do
+  let
+    Just i = lookup s exports
+    imCount = length imports
+    run vm@VM{globs, stack, insts, mem} = case head insts of
+      (Call i:rest) -> if i < imCount then print "TODO: call imported function"
+        else do
+          let Body _ os = code!!(i - imCount)
+          run vm { insts = os:rest:tail insts }
+      (Set_global i:rest) -> run vm {globs = IM.insert i (head stack) globs, stack = tail stack, insts = rest:tail insts}
+      (Get_global i:rest) -> run vm {stack = globs IM.! i:stack, insts = rest:tail insts}
+      (c@(I32_const _):rest) -> run vm {stack = c:stack, insts = rest:tail insts}
+      (c@(I64_const _):rest) -> run vm {stack = c:stack, insts = rest:tail insts}
+      (I32_add:rest) -> let
+        I32_const a = stack!!1
+        I32_const b = head stack
+        c = I32_const $ a + b
+        in run vm {stack = c:drop 2 stack, insts = rest:tail insts}
+      (I32_sub:rest) -> let
+        I32_const a = stack!!1
+        I32_const b = head stack
+        c = I32_const $ a - b
+        in run vm {stack = c:drop 2 stack, insts = rest:tail insts}
+      (I32_load _ _:rest) -> do
+        let
+          I32_const addr = head stack
+          c = I32_const $ fromIntegral $ getNum 4 addr mem
+        run vm {stack = c:tail stack, insts = rest:tail insts}
+      (I32_store _ _:rest) -> do
+        let
+          I32_const addr = stack!!1
+          I32_const n = head stack
+        run vm {stack = drop 2 stack, insts = rest:tail insts, mem = putNum 4 addr n mem}
+      (I64_store _ _:rest) -> do
+        let
+          I32_const addr = stack!!1
+          I64_const n = head stack
+        run vm {stack = drop 2 stack, insts = rest:tail insts, mem = putNum 8 addr n mem}
+      _ -> error $ show vm
+  if i < imCount then
+    print "TODO: call imported function"
+  else do
+    let Body _ os = code!!(i - imCount)
+    run $ VM (IM.fromList $ zip [0..] $ head . snd <$> globals) [] [os] IM.empty
